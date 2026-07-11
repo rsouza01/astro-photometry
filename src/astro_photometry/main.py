bd62282d2480fd049710c6bdc7eb1b8cebfb7214
Rodrigo de Souza <rsouza01@gmail.com>
2026-07-11 15:18:30 +0200

    Added entrypoint

---
+++ src/astro_photometry/main.py
@@ -0,0 +1,10 @@
+# src/astro_photometry/main.py
+
+def main():
+    print("Hello from your project!")
+    # Your main logic, like calling your TOV solver, goes here
+    # import numpy as np
+    # from astropy import units as u
+
+if __name__ == "__main__":
+    main()

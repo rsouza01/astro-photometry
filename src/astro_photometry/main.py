0abf11acb445369fbae9699fb9edd9cca6b885c9
Rodrigo de Souza <rsouza01@gmail.com>
2026-07-11 15:51:15 +0200

    Integrating modules

--- src/astro_photometry/main.py
+++ src/astro_photometry/main.py
@@ -1,10 +1,23 @@
 # src/astro_photometry/main.py
 
+import numpy as np
+from astropy import units as u
+from astropy import constants as const
+
+from astro_photometry.star import Star
+
 def main():
-    print("Hello from your project!")
-    # Your main logic, like calling your TOV solver, goes here
-    # import numpy as np
-    # from astropy import units as u
+
+    # Initialize the Sun
+    sun = Star(radius=6.957e8 * u.m, 
+            temperature=5778 * u.K, 
+            name="Sun")
+
+    # Calculate and verify
+    l_sun = sun.calculate_luminosity()
+    print(f"Calculated Luminosity of {sun.name}: {l_sun:.3e}")
+    print(f"Expected Luminosity: 3.828e+26 W")
+
 
 if __name__ == "__main__":
     main()
--- src/astro_photometry/star.py
+++ src/astro_photometry/star.py
@@ -1,3 +1,5 @@
+# src/astro_photometry/star.py
+
 import numpy as np
 from astropy import units as u
 from astropy import constants as const
@@ -19,13 +21,3 @@ class Star:
         # Luminosity
         luminosity = area * sigma * (self.temperature**4)
         return luminosity.to(u.W)
-
-# Initialize the Sun
-sun = Star(radius=6.957e8 * u.m, 
-           temperature=5778 * u.K, 
-           name="Sun")
-
-# Calculate and verify
-l_sun = sun.calculate_luminosity()
-print(f"Calculated Luminosity of {sun.name}: {l_sun:.3e}")
-print(f"Expected Luminosity: 3.828e+26 W")
